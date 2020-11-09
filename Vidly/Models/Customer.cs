e4f19a7f7f98d313c7dd9430949cc2660652d0da
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-09 14:49:34 +0530

    I will go for way 1

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -17,6 +17,7 @@ namespace Vidly.Models
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
 
+        [Display(Name="Date Of Birth")]
         public DateTime? DOB { get; set; }
     }
 
