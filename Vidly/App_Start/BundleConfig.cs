cb2f180f26acc7d0ba4f791f8b685911c81ae2cf
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-20 12:07:15 +0530

    auto fill for customers

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace Vidly
                         "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/DataTables/dataTables.bootstrap.js",
-                        "~/Scripts/DataTables/jquery.dataTables.js"
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/typeahead.bundle.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -30,6 +31,7 @@ namespace Vidly
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/Site.css",
+                      "~/Content/typeahead.css",
                       "~/Content/DataTables/css/dataTables.bootstrap.css"
             ));
 
